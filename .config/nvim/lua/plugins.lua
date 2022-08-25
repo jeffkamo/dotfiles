d001822a0cbb1e36ee06fee2771b2a1166803a85
Jeff Kamo <jeff.kamo@clio.com>
2022-08-24 22:46:18 -0700

    add treesitter pack and attempt to update after installing plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -21,6 +21,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
     install_path })
   vim.cmd [[packadd packer.nvim]]
+  vim.cmd [[packadd nvim-treesitter]]
 end
 
 local packer = require "packer"
