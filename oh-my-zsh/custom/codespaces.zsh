ce8bcee173c6e019a4b60251adb67147612f7cf4
Jeff Kamo <admin@jeffkamo.com>
2023-06-15 11:07:06 -0700

    Remove unnecessary environment specific configs (the macosx one caused hard to find problems)

--- oh-my-zsh/custom/codespaces.zsh
+++
@@ -1,2 +0,0 @@
-# Allow bundler to authenticate with Artifactory. Note that this is handled by the Clio 'dev' tool on local environments
-export BUNDLE_CLIO__JFROG__IO="${ARTIFACTORY_USERNAME}:${ARTIFACTORY_API_KEY}"
--- oh-my-zsh/custom/macos.zsh
+++
@@ -1,6 +0,0 @@
-if command -v dev &>/dev/null; then
-  eval "$(dev _hook)"
-fi
-
-# export local bin after rbenv inits so we can override the shims (for bin/rspec, bin/rails, etc)
-export PATH="./bin:$PATH" 
